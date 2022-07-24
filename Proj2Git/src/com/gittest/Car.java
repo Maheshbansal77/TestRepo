21bea8cd07a10e44d529b12d08b72dafd7939b61
Maheshbansal77 <bansal_mahesh@yahoo.com>
2022-07-24 19:04:24 +0530

    commit for 2nd time

--- Proj2Git/src/com/gittest/Car.java
+++ Proj2Git/src/com/gittest/Car.java
@@ -4,6 +4,7 @@ public class Car {
 
 	public static void main(String[] args) {
 		System.out.println("Test for GIT");
+		System.out.println("This is second line");
 
 	}
 
---
+++ Proj2Git/src/com/gittest/Engine.java
@@ -0,0 +1,10 @@
+package com.gittest;
+
+public class Engine {
+
+	public static void main(String[] args) {
+		System.out.println("This is a engine class for GIT");
+
+	}
+
+}
